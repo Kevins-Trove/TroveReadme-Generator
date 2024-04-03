c720424ced0ec625faf10f1995ed599618b3675f
Kevin Roper <kevin@kevinstrove.com>
2024-04-02 21:50:54 -0700

    Layout of sturcture for README

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 // TODO: Include packages needed for this application
-const 
+const inquirer = require('inquirer');
+const inquirer = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
+
+function test() {
+    console.log(questions);
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    prompt(question).then()
+    //prompt(question).then()
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Project title?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Project description?'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Installation instructions:'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Usage information:'
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Contribution guidelines:'
+    }
+    ,
+    {
+      type: 'input',
+      name: 'test',
+      message: 'test instructions:'
+    }
+  ])
+  .then((answers) => {
+    questions.push(answers);
+    test();
+    });
 }
+
+
 
 // Function call to initialize app
 init();
